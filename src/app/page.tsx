f7e393af13bf20296f9e37c7c9e2cbefaeda9b41
Abhijit Roy <abhijit@pop-os.localdomain>
2023-11-01 18:05:26 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 
+import React from 'react';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
-       <div className="flex flex-col items-center justify-center"></div>
-        <p className="text-4xl font-bold text-center">Botai</p>
-        <p className="text-2xl font-bold text-center">A bot that helps you to manage your tasks</p>
-      <div  className=""></div>
-      
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 relative bg-gradient-to-b from-blue-200 to-blue-400">
+      <div className="text-center text-white">
+        <p className="text-4xl font-bold pb-8">Botai</p>
+        <p className="text-2xl font-bold pb-8">
+          A bot that helps you manage your tasks
+        </p>
+        <a href="/dashboard" className="mt-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Get Started
+          </button>
+        </a>
+      </div>
     </main>
   );
 }
+
 
