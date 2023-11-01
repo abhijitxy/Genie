ede89c8ee17e6adfeb9ab6f59eb50b3f2bcf11fb
Abhijit20112003 <roya51788@gmail.com>
2023-11-01 22:46:10 +0530

    remove it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
           A bot that helps you manage your tasks
         </p>
         <a href="/dashboard" className="mt-4">
-          //test 
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Get Started
           </button>
