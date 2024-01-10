99dc12eeb80a1e86a6ec0fb94faf7b7aca6932ef
Abhijit Roy <roya51788@gmail.com>
2024-01-10 13:00:04 +0530

    remove tailwind congfig.js

--- src/app/(dashboard)/integrations /page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page () {    
-    return (
-        <main>
-            <div className="flex flex-col items-center justify-between p-7">
-            Integrations
-            </div>
-        </main>
-    )
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+ import Navbar from '@/components/Section/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        
+        <Navbar/>
         {children}
         </body>
     </html>
