2db63cf3796be8dd34ea61da4da9faf31690ef38
Abhijit Roy <roya51788@gmail.com>
2024-01-12 16:54:56 +0530

    adding create page

--- src/components/Section/Navbar.tsx
+++ src/components/Section/Navbar.tsx
@@ -17,9 +17,11 @@ const Navbar = () => {
             Integrations
           </Link>
         </div>
-        <div className="inline-flex h-10 items-center justify-center text-white rounded-md bg-gray-900 px-8 text-sm font-medium shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50">
-          <Link href={"/chatbots"}>Login</Link>
-        </div>
+        <a href="/chatbots">
+          <div className="inline-flex h-10 items-center justify-center text-white rounded-md bg-gray-900 px-8 text-sm font-medium shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-gray-950">
+            Login
+          </div>
+        </a>
       </div>
     </header>
   );
