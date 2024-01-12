8687e7aa85c1f4d60833d3a695cbb7e99e3263c7
Abhijit Roy <roya51788@gmail.com>
2024-01-12 17:00:54 +0530

    updating middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,15 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
- 
-export const config = {
-  matcher: ["/chatbots"],
-};
+import withAuth from "next-auth/middleware"
+
+export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    function middleware(req) {
+      console.log(req.nextauth.token)
+    },
+    {
+      callbacks: {
+        authorized: ({ token }) => token?.role === "admin",
+      },
+    }
+  )
+
+export const config = { matcher: ["/login"] }
