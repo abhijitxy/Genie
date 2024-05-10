afae943f2b14f418ef005983b41e96b6b30f3d15
Abhijit Roy <roya51788@gmail.com>
2024-05-10 22:48:30 +0530

    migrate to api route

--- src/app/(dashboard)/Components/bot.tsx
+++ src/app/(dashboard)/Components/bot.tsx
@@ -1,72 +1,29 @@
 "use client";
 
-import { type CoreMessage } from "ai";
-import { useState } from "react";
-import { continueConversation } from "@/app/action";
-import { readStreamableValue } from "ai/rsc";
+import { useChat } from 'ai/react';
 
-export default function Chat() {
-  const [messages, setMessages] = useState<CoreMessage[]>([]);
-  const [input, setInput] = useState("");
+import React from 'react'
+
+function bot() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex flex-col h-[700px] w-80 overflow-hidden items-center shadow-xl rounded-lg">
-      <div className="w-full text-left px-4 py-2 bg-black">
-        <span className="text-2xl font-bold text-white">GENIE</span>
-        <span className="block text-sm text-white opacity-75">Welcome!</span>
-      </div>
-      <div className="flex-grow p-4 overflow-auto">
-        {messages.length > 0 ? (
-          messages.map((m, i) => (
-            <div key={i} className={`flex mb-4 ${m.role === 'user' ? 'justify-end' : 'justify-start'}`}>
-              <div
-                className={`max-w-2/3 rounded-lg shadow ${
-                  m.role === "user"
-                    ? "bg-blue-500 text-white"
-                    : "bg-gray-200 text-gray-800"
-                }`}
-              >
-                <div className="px-4 py-2">
-                  <span className="text-sm">{m.content as string}</span>
-                </div>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="py-2 px-4 rounded-lg bg-gray-200 text-black">
-            Welcome to this awesome chat!
-          </div>
-        )}
-      </div>
-      <form
-        action={async () => {
-          const newMessages: CoreMessage[] = [
-            ...messages,
-            { content: input, role: "user" },
-          ];
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map(m => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content}
+        </div>
+      ))}
 
-          setMessages(newMessages);
-          setInput("");
-
-          const result = await continueConversation(newMessages);
-
-          for await (const content of readStreamableValue(result)) {
-            setMessages([
-              ...newMessages,
-              {
-                role: "assistant",
-                content: content as string,
-              },
-            ]);
-          }
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <input
-          className="w-full p-3 border border-gray-300 shadow rounded-full"
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
-          onChange={(e) => setInput(e.target.value)}
+          onChange={handleInputChange}
         />
       </form>
     </div>
   );
 }
+export default bot
--- src/app/action.ts
+++ src/app/action.ts
@@ -1,20 +1,5 @@
 'use server';
-
-import { createStreamableValue } from 'ai/rsc';
-import { CoreMessage, streamText } from 'ai';
-import { openai } from '@ai-sdk/openai';
 import prisma from "@/app/lib/db"
-
-
-export async function continueConversation(messages: CoreMessage[]) {
-  const result = await streamText({
-    model: openai('gpt-4-turbo'),
-    messages,
-  });
-
-  const stream = createStreamableValue(result.textStream);
-  return stream.value;
-}
 
 export async function Chatbot(formData: FormData) {
   const chatbot = await prisma.chatbot.create({
